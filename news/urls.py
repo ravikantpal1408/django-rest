65671a8a14bcce7bcac3407647cc461b8c5a98bf
ravikantpal1408 <ravikantpal123@gmail.com>
2019-12-29 02:25:04 +0530

    implemented class based view

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from news.views import article_list_create_api_view, article_detail_api_view
+# from news.views import article_list_create_api_view, article_detail_api_view
+from news.views import ArticleListCreateAPIView, ArticleDetailAPIView
+
+#   now implementing the class based view url scheme
 
 urlpatterns = [
-    path('v1/articles/', article_list_create_api_view, name='article-list'),
-    path('v1/articles/<int:pk>', article_detail_api_view, name='article-detail'),
+    path('v1/articles/', ArticleListCreateAPIView.as_view(), name='article-list'),
+    path('v1/articles/<int:pk>',
+         ArticleDetailAPIView.as_view(), name='article-detail'),
+    # path('v1/articles/', article_list_create_api_view, name='article-list'),
+    # path('v1/articles/<int:pk>', article_detail_api_view, name='article-detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,9 @@
 # from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
 # calling models
 from news.models import Article
@@ -48,3 +50,49 @@ def article_detail_api_view(request, pk):
 
     else:
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+# class based view api response
+class ArticleListCreateAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.filter(active=True)
+        serializer = ArticleSerializers(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    pass
+
+# class based view api response
+
+
+class ArticleDetailAPIView(APIView):
+
+    def get_object(self, pk):
+        article = get_object_or_404(Article, pk)
+        return article
+
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializers(article)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializers(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        article = self.get_object(pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    pass
